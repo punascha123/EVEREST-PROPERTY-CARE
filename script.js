0f0448c932d97988080bcd9522b803bad40792a4
punascha123 <binod.ghimire164@gmail.com>
2025-07-26 13:39:12 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+let current = 0;
+const testimonials = document.querySelectorAll('.testimonial-card');
+
+function showTestimonial(index) {
+  testimonials.forEach((card, i) => {
+    card.classList.remove('active');
+    if (i === index) {
+      card.classList.add('active');
+    }
+  });
+}
+
+function nextTestimonial() {
+  current = (current + 1) % testimonials.length;
+  showTestimonial(current);
+}
+
+function prevTestimonial() {
+  current = (current - 1 + testimonials.length) % testimonials.length;
+  showTestimonial(current);
+}
+
+setInterval(nextTestimonial, 6000);
 
